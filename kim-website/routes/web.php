894879240cc4460ca0712fcd6c811a6fb18ddcdb
FARHAN MUZHAFFAR TIRAS PUTRA <96176429+Hanhan73@users.noreply.github.com>
2026-01-16 02:14:25 +0700

    Update web.php

--- kim-website/routes/web.php
+++ kim-website/routes/web.php
@@ -439,7 +439,7 @@ Route::prefix('admin/digital')->name('admin.digital.')->group(function () {
 });
 
 // Admin Routes - Protected by admin middleware
-Route::prefix('admin/digital')->name('admin.digital.')->middleware(['digital_auth'])->group(function () {
+Route::prefix('admin/digital')->name('admin.digital.')->middleware(['check.digital.role:collaborator'])->group(function () {
 
     // Dashboard
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
